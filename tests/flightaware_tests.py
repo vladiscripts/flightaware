a0af4dfbaedde79e651cea046327531b2560fdd7
Ed Crump <ed.crump@gmail.com>
2015-09-11 17:50:07 -0700

    OCD cleanup.

--- tests/flightaware_tests.py
+++ tests/flightaware_tests.py
@@ -21,28 +21,9 @@ class TestSequenceFunctions(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_fleet_arrived(self):
-        results = self.client.fleet_arrived("URF")
-        pprint(results)
-        self.assertNotIn("error", results)
-
-    def test_decode_route(self):
-        results = self.client.decode_route("KSQL", "SJC V334 SAC SWR", "KTRK")
-        pprint(results)
-        self.assertNotIn("error", results)
-
-    def test_flight_info(self):
-        results = self.client.flight_info("N415PW")
-        print results
-        self.assertNotIn("error", results)
-
-    def test_metar(self):
-        results = self.client.metar("BNA")
-        self.assertNotIn("error", results)
-
-    def test_metar_ex(self):
-        results = self.client.metar_ex("BNA")
-        self.assertNotIn("error", results)
+    #
+    # Combined API tests
+    #
 
     def test_basic_calls(self):
         results = self.client.all_airlines()
@@ -51,6 +32,16 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertNotIn("error", results)
         results = self.client.count_airport_operations("BNA")
         self.assertNotIn("error", results)
+
+    def weather_calls(self):
+        results = self.client.ntaf("BNA")
+        self.assertNotIn("error", results)
+        results = self.client.taf("BNA")
+        self.assertNotIn("error", results)
+
+    #
+    # Individual API tests
+    #
 
     def test_aircraft_type(self):
         results = self.client.aircraft_type("GALX")
@@ -75,14 +66,31 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertNotIn("error", results)
         print results
 
-    def weather_calls(self):
-        results = self.client.ntaf("BNA")
-        self.assertNotIn("error", results)
-        results = self.client.taf("BNA")
+    def test_all_airlines(self):
+        results = self.client.all_airlines()
         self.assertNotIn("error", results)
 
-    def test_zipcode_info(self):
-        results = self.client.zipcode_info("37221")
+    def test_all_airports(self):
+        results = self.client.all_airports()
+        self.assertNotIn("error", results)
+
+    def test_count_airport_operations(self):
+        results = self.client.count_airport_operations("BNA")
+        self.assertNotIn("error", results)
+
+    def test_decode_route(self):
+        results = self.client.decode_route("KSQL", "SJC V334 SAC SWR", "KTRK")
+        pprint(results)
+        self.assertNotIn("error", results)
+
+    def test_fleet_arrived(self):
+        results = self.client.fleet_arrived("URF")
+        pprint(results)
+        self.assertNotIn("error", results)
+
+    def test_flight_info(self):
+        results = self.client.flight_info("N415PW")
+        print results
         self.assertNotIn("error", results)
 
     def test_get_flight_info(self):
@@ -100,4 +108,16 @@ class TestSequenceFunctions(unittest.TestCase):
         for result in results:
             self.assertIn("arrival_time", result)
             self.assertIn("departure_time", result)
+
+    def test_metar(self):
+        results = self.client.metar("BNA")
+        self.assertNotIn("error", results)
+
+    def test_metar_ex(self):
+        results = self.client.metar_ex("BNA")
+        self.assertNotIn("error", results)
+
+    def test_zipcode_info(self):
+        results = self.client.zipcode_info("37221")
+        self.assertNotIn("error", results)
 
